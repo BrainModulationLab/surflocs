e24576a0dcc4e7ebb688986fd427092b9392f8ee
neuropsyched <arikappel@gmail.com>
2016-10-26 16:30:16 -0400

    Add files via upload
    commit subdirectories

---
+++ external/dbs_libs_helper.m
@@ -0,0 +1,19 @@
+function dbs_libs_helper(libpath)
+
+% add shared libraries to search path
+
+if nargin < 1
+    libpath = fileparts(mfilename('fullpath'));
+end
+
+if ismac
+    envname = 'DYLD_LIBRARY_PATH';
+elseif isunix
+    envname = 'LD_LIBRARY_PATH';
+elseif ispc
+    envname = 'PATH';
+end
+
+if isempty(strfind(getenv(envname),libpath))
+    setenv(envname, [libpath, ';', getenv(envname)]);
+end
---
+++ external/dcm2nii/dcm2nii.m
@@ -0,0 +1,35 @@
+function dcm2nii(inputimage)
+% Wrapper for dcm2nii, just used for reorientation and cropping currently
+
+dbs_libs_helper;
+
+basedir = [fileparts(mfilename('fullpath')), filesep];
+
+if ispc
+    dcm2nii = [basedir, 'dcm2nii.exe'];
+else
+    dcm2nii = [basedir, 'dcm2nii.', computer('arch')];
+end
+
+cmd=[dcm2nii, ' -g n -x y ', inputimage];
+
+display(sprintf('\nReorient and crop image...'))
+if ~ispc
+    system(['bash -c "', cmd, '"']);
+else
+    system(cmd);
+end
+
+[pth,fn,ext]=fileparts(inputimage);
+if isempty(pth)
+    pth = '.';
+end
+
+if exist([pth,filesep,'co',fn,ext], 'file')
+    movefile([pth,filesep,'co',fn,ext],inputimage);
+    delete([pth,filesep,'o',fn,ext]);
+elseif exist([pth,filesep,'c',fn,ext], 'file')
+    movefile([pth,filesep,'c',fn,ext],inputimage);
+else
+	disp('No need to cropping!');
+end
